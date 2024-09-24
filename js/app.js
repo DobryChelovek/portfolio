b515c624524be662e2077c1e13dac4c9b373d8e2
Margo <margo.simik@yandex.ru>
2024-09-24 15:40:56 +0300

    исправлена ошибка почты и телеги

--- js/app.js
+++ js/app.js
@@ -2,6 +2,6 @@ let el = document.getElementById("margo-simik")
 el.innerHTML = "margo.simik@yandex.ru"
 el.href = "mailto:margo.simik@yandex.ru" 
 
-let el = document.getElementById("telegram-margo")
-el.innerHTML = "telegram"
-el.href = "https://t.me/Azal4118" 
+let telegram = document.getElementById("telegram-margo")
+telegram.innerHTML = "telegram"
+telegram.href = "https://t.me/Azal4118" 
