069328c6b214de3b9c311de7dace8ee17898986e
Margo <margo.simik@yandex.ru>
2024-09-24 15:40:40 +0300

    скрыта почта и телега

---
+++ js/app.js
@@ -0,0 +1,7 @@
+let el = document.getElementById("margo-simik")
+el.innerHTML = "margo.simik@yandex.ru"
+el.href = "mailto:margo.simik@yandex.ru" 
+
+let el = document.getElementById("telegram-margo")
+el.innerHTML = "telegram"
+el.href = "https://t.me/Azal4118" 
