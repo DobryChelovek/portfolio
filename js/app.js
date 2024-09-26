f655df00f63af6f7a16160076bed8300c12d8457
Margo <margo.simik@yandex.ru>
2024-09-26 20:08:37 +0300

    сделан адаптив header сделана структура второй страницы

--- js/app.js
+++ js/app.js
@@ -5,3 +5,19 @@ el.href = "mailto:margo.simik@yandex.ru"
 let telegram = document.getElementById("telegram-margo")
 telegram.innerHTML = "telegram"
 telegram.href = "https://t.me/Azal4118" 
+
+let elMobile = document.getElementById("margo-simik-m")
+elMobile.innerHTML = "margo.simik@yandex.ru"
+elMobile.href = "mailto:margo.simik@yandex.ru" 
+
+let telegramMobile = document.getElementById("telegram-margo-m")
+telegramMobile.innerHTML = "telegram"
+telegramMobile.href = "https://t.me/Azal4118" 
+
+let elFooter = document.getElementById("margo-simik-footer")
+elFooter.innerHTML = "margo.simik@yandex.ru"
+elFooter.href = "mailto:margo.simik@yandex.ru" 
+
+let telegramFooter = document.getElementById("telegram-margo-footer")
+telegramFooter.innerHTML = "telegram"
+telegramFooter.href = "https://t.me/Azal4118" 
